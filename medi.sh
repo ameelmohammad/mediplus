62f9ac12046b0d26a69ce576c53d13f24833a9c5
ameelmohammad <77770908+ameelmohammad@users.noreply.github.com>
2023-11-29 11:27:17 +0530

    Update medi.sh

--- medi.sh
+++ medi.sh
@@ -6,16 +6,12 @@
 # Team :AJA-DevOps
 #####################################
 echo "system update"
-sudo apt-get update 
+sudo yum update 
 echo "install nginx"
-sudo apt-get install nginx -y
-echo "install zip"
-sudo apt-get install zip -y
-echo "install unzip"
-sudo apt-get install unzip -y
+sudo yumt install httpd -y
 echo "download mediplus-lite"
 wget https://www.free-css.com/assets/files/free-css-templates/download/page296/mediplus-lite.zip
 echo "unzip mediplus"
 sudo unzip mediplus-lite.zip
 echo "move to medplus-lite file in nginx server"
-sudo mv mediplus-lite /var/www/html
+sudo mv -f mediplus-lite /var/www/html
